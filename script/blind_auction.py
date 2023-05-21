3e79d5d42da1c226c3dc48e0fad7ef3f98ffcfac
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-05-21 15:46:21 +0100

    commit

--- script/blind_auction.py
+++ script/blind_auction.py
@@ -3,6 +3,11 @@ import time
 dictionary = {}
 print('welcome to the blind auction program')
 time.sleep(1)
-name = input("what's your name? : ")
-bid = int(input("what's your bid: "))
-
+is_next = True
+while is_next:
+    name = input("what's your name? : ")
+    bid = int(input(f"what's your bid {name}: "))
+    dictionary[name] = bid
+    is_bidder = input("is there another bidder?\nEnter 'y' for yes and 'n' for no: ")
+    if is_bidder.lower() in ['n', 'no']:
+        is_next = False
